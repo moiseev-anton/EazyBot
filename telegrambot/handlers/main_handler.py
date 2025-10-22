129b5f00346944131f35914ed6233ce1f841a84f
anton moiseev <moucee13@gmail.com>
2025-10-23 00:30:48 +0300

    adapted main_handler for the keyboard retrieval method

--- telegrambot/handlers/main_handler.py
+++ telegrambot/handlers/main_handler.py
@@ -1,10 +1,10 @@
-from aiogram import Router, types, F
+from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 from dependency_injector.wiring import inject, Provide
 
 from dependencies import Deps
-from managers import MessageManager, KeyboardManager
 from enums import NavigationAction
+from managers import KeyboardManager, MessageManager
 from services import UserService
 
 router = Router()
@@ -18,10 +18,15 @@ async def main_handler(
         user_service: UserService = Provide[Deps.services.user],
 ):
     user = await user_service.get_user_with_subscriptions()
+    sub_id = endpoint = None
+    if user.subscriptions:
+        first_subscription = user.subscriptions[0]
+        sub_id = first_subscription.id
+        endpoint = first_subscription.link
 
     await callback.message.edit_text(
         text=MessageManager.get_main_message(user),
-        reply_markup=KeyboardManager.get_main_keyboard(user),
+        reply_markup=KeyboardManager.get_main_keyboard(sub_id, endpoint),
     )
 
     await state.clear()
